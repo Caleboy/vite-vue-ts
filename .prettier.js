6044fc267bd0c7439709492a8f2cbb7ad24c6c12
caibingbing <caibingbing@mysteel.com>
2022-03-15 14:36:46 +0800

    build: eslint

--- .prettier.js
+++ .prettier.js
@@ -1,17 +1,19 @@
 module.exports = {
   tabWidth: 2,
-  jsxSingleQuote: false,
+  jsxSingleQuote: true,
   jsxBracketSameLine: true,
-  printWidth: 100,
-  singleQuote: false,
+  singleQuote: true,
   semi: false,
+  printWidth: 100,
+  insertPragma: false,
+  trailingComma: 'none',
   overrides: [
     {
-      files: "*.json",
+      files: '*.json',
       options: {
         printWidth: 200,
       },
     },
   ],
-  arrowParens: "always",
-};
+  arrowParens: 'always',
+}
